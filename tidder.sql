7e45bd0b3942029cdd79cd4174cf7ed3fdd30305
Rekicho <up201604145@fe.up.pt>
2018-11-22 09:24:57 +0000

    First Possible Database Implementation

---
+++ tidder.sql
@@ -0,0 +1,41 @@
+--NOT TESTED
+
+CREATE TABLE user (
+    user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    salt VARCHAR NOT NULL, --MUDAR VARCHAR PARA TAMANHO CERTO QUANDO FIZERMOS A PARTE DE SECURE PASSWORD
+    realname VARCHAR,
+    email VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE post (
+    post_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    post_title VARCHAR NOT NULL, --TAMANHO DE TITULO MÁXIMO?
+    post_text VARCHAR NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES user
+);
+
+--STILL NEEDS COMMENT TO COMMENT
+CREATE TABLE comment (
+    comment_id INTEGER NOT NULL, --AUTOINCREMENT?
+    post_id INTEGER NOT NULL REFERENCES post,
+    comment_text VARCHAR NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES user,
+    PRIMARY KEY(comment_id,post_id)
+);
+
+--MAYBE DO VOTE_COMMENT AND VOTE_POST ON SAME TABLE?
+CREATE TABLE vote_post(
+    post_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    value INTEGER NOT NULL CHECK (value = -1 OR value = 1),
+    PRIMARY KEY(post_id,user_id)
+);
+
+CREATE TABLE vote_comment(
+    comment_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    value INTEGER NOT NULL CHECK (value = -1 OR value = 1),
+    PRIMARY KEY(comment_id,user_id)
+);
