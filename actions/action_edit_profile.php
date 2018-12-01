bdf491fbbf9e6a7edbdb4a92c36084808fdb4da9
Francisco Filipe <francilipe.7@gmail.com>
2018-12-01 23:25:43 +0000

    Fixed bug related to profile pics and redirection in edit profile

--- actions/action_edit_profile.php
+++ actions/action_edit_profile.php
@@ -3,7 +3,8 @@
 	include_once('../database/db_user.php');
 
 	$user_id = $_SESSION['user_id'];
-	$username = $_POST['username'];
+	$curr_username = getUserName($user_id); //if the new username is already taken redirect using this username
+	$new_username = $_POST['username'];
 	$realname = $_POST['realname'];
 	$email = $_POST['email'];
 	$birthday = $_POST['birthday'];
@@ -12,18 +13,24 @@
 	$img_file = $_FILES['img']['tmp_name'];
 	$ext = pathinfo($_FILES['img']['name'], PATHINFO_EXTENSION);
 
-	if(file_exists("../pictures/".$username.".jpeg"))
-		unlink("../pictures/".$username.".jpeg");
-	else if(file_exists("../pictures/".$username.".jpg"))
-		unlink("../pictures/".$username.".jpg");
-	else
-		unlink("../pictures/".$username.".png");
-
 	try {
-		move_uploaded_file($img_file, "../pictures/".$username.".".$ext);
-		editUserInfo($user_id, $username, $realname, $email, $birthday, $bio);
-        header('Location: ../pages/profile.php?username='.urlencode($username));
+		editUserInfo($user_id, $new_username, $realname, $email, $birthday, $bio);
+		updateProfilePic($img_file, $curr_username, $new_username, $ext);
+        header('Location: ../pages/profile.php?username='.urlencode($new_username));
     } catch (PDOException $e) {
-        header('Location: ../pages/profile.php?username='.urlencode($username));
-    }
+        header('Location: ../pages/profile.php?username='.urlencode($curr_username));
+	}
+	
+	function updateProfilePic($img_file, $curr_username, $new_username, $ext){
+		if($img_file != "")
+		{
+			$profile_pics =  glob("../pictures/".$curr_username.".*");
+
+			foreach($profile_pics as $pic)
+				unlink($pic);
+
+			move_uploaded_file($img_file, "../pictures/".$new_username.".".$ext);
+		}
+	}
 ?>
+
