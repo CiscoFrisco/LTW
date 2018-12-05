347d5cd5beec7ee51a4edf754668ad952e328f59
Rekicho <up201604145@fe.up.pt>
2018-12-05 22:22:10 +0000

    User score done

--- pages/profile.php
+++ pages/profile.php
@@ -18,6 +18,30 @@
 	if($user_id == '')
 		header('Location: ../pages/404.html');
 
+	$stories = getStories($user_id);
+	$comments = getComments($user_id);
+	$now = time();
+
+	$score = 0;
+
+	for($i = 0; $i < count($stories); $i++){
+		$stories[$i]['username'] = $username;
+		$stories[$i]['score'] = getScore($stories[$i]['opinion_id']);
+		$score += $stories[$i]['score'];
+
+		if(isset($_SESSION['user_id']))
+			$stories[$i]['vote'] = getVote($stories[$i]['opinion_id'], $_SESSION['user_id']);
+	}
+
+	for($i = 0; $i < count($comments); $i++){
+		$comments[$i]['username'] = $username;
+		$comments[$i]['score'] = getScore($comments[$i]['opinion_id']);
+		$score += $comments[$i]['score'];
+
+		if(isset($_SESSION['user_id']))
+			$comments[$i]['vote'] = getVote($comments[$i]['opinion_id'], $_SESSION['user_id']);
+	}
+
 	$formatted_birthday = formatDate($birthday);
 	$formatted_join_date = formatDate($join_date);
 	
@@ -41,76 +65,58 @@
 	<h2><?=$username?> Profile</h2>
 	<h3><img src=<?=$path?> alt=<?=$alt?> width="100" height="100"></h3>
 	<h3>Username: <?=$username?></h3>
+	<h3>Score: <?=$score?></h3>
 	<h3>Name: <?=$realname?></h3>
 	<h3>Bio: <?=$bio?></h3>
 	<h3>Email: <?=$email?></h3>
 	<h3>Birthday: <?=$formatted_birthday?></h3>
 	<h3>Join Date: <?=$formatted_join_date?></h3>
 
-	<?php
+<?php
 	if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $user_id){?>
 
-	<section id="edit">
-		<header>
-			<h2>Edit</h2>
-		</header>
-		<form method="post" action="../actions/action_edit_profile.php" enctype="multipart/form-data">
-			<label>Profile Picture:
-				<input type="file" name="img" value="" accept="image/png, image/jpeg">
-			</label>
-			<label>Username:
-				<input type="text" name="username" value="<?=$username?>" required>
-			</label>	
-			<label>Name:
-				<input type="text" name="realname" value="<?=$realname?>">
-			</label>
-			<label>Email:
-				<input type="email" name="email" value="<?=$email?>" required>
-			</label>
-			<label>Birthday:
-				<input type="date" name="birthday" value="<?=$birthday?>">
-			</label>
-			<label>Bio:
-				<textarea name="bio"><?=$bio?></textarea>
-			</label>
-				
-			<input type="submit" value="Edit Profile">
-		</form>
-	</section>
-	<?php } ?>
-	<section id="stories">
-		<?php 
-			$user_id = getUserID($username);
-			$stories = getStories($user_id);
-			$comments = getComments($user_id);
-			$now = time();
+<section id="edit">
+	<header>
+		<h2>Edit</h2>
+	</header>
+	<form method="post" action="../actions/action_edit_profile.php" enctype="multipart/form-data">
+		<label>Profile Picture:
+			<input type="file" name="img" value="" accept="image/png, image/jpeg">
+		</label>
+		<label>Username:
+			<input type="text" name="username" value="<?=$username?>" required>
+		</label>
+		<label>Name:
+			<input type="text" name="realname" value="<?=$realname?>">
+		</label>
+		<label>Email:
+			<input type="email" name="email" value="<?=$email?>" required>
+		</label>
+		<label>Birthday:
+			<input type="date" name="birthday" value="<?=$birthday?>">
+		</label>
+		<label>Bio:
+			<textarea name="bio"><?=$bio?></textarea>
+		</label>
 
-			for($i = 0; $i < count($stories); $i++){
-				$stories[$i]['username'] = $username;
-				$stories[$i]['score'] = getScore($stories[$i]['opinion_id']);
-		
-				if(isset($_SESSION['user_id']))
-					$stories[$i]['vote'] = getVote($stories[$i]['opinion_id'], $_SESSION['user_id']);
-			}
+		<input type="submit" value="Edit Profile">
+	</form>
+</section>
+<?php } ?>
+<section id="stories">
+	<?php 
+	draw_stories($stories, false);
+?>
+</section>
+<section id="comments">
+	<?php 
+	draw_comments($comments,false);
+?>
+</section>
+<section id="logout">
+	<header>
+		<h4><a href="../actions/action_logout.php">Logout</a></h4>
+	</header>
+</section>
 
-			for($i = 0; $i < count($comments); $i++){
-				$comments[$i]['username'] = $username;
-				$comments[$i]['score'] = getScore($comments[$i]['opinion_id']);
-		
-				if(isset($_SESSION['user_id']))
-					$comments[$i]['vote'] = getVote($comments[$i]['opinion_id'], $_SESSION['user_id']);
-			}
-
-			draw_stories($stories, false);
-			draw_comments($comments,false);
-		?>
-	</section>
-	<section id="comments">
-	</section>
-	<section id="logout">
-		<header>
-			<h4><a href="../actions/action_logout.php">Logout</a></h4>
-		</header>
-	</section>
-
-	<?php draw_footer(); ?>
+<?php draw_footer(); ?>
