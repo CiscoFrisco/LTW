dd0c15dc9f5acc35dad435d637a1eb7aebc611d9
Pedro Silva <p21silva98@outlook.com>
2018-12-13 22:25:07 +0000

    some more css

--- pages/add_story.php
+++ pages/add_story.php
@@ -17,24 +17,24 @@
 	$channels = getAllChannels();		
 	draw_header(true);
 ?>
-
-	<section id="new_story">
-		<header>
-			<h2>Add a New Story</h2>
-		</header>
-		<form method="post" action="../actions/action_add_story.php">
-			<input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>">
-			<input type="text" name="title" placeholder="Title" required>
-			<textarea name="story" placeholder="Story" required></textarea>
-			<?php draw_channel_options($channels); ?>
-			<input type="submit" value="Add Story">
-		</form>
-		<?php if(isset($_GET['error'])){ 
-				if($_GET['error'] == 'title'){?>
-					<h3>Title is too long! (Max is 50 characters)</h3>
-		<?php } else if($_GET['error'] == 'text') {?>
-					<h3>Story is too long! (Max is 50000 characters)</h3>
-		<?php }} ?>
-	</section>
-	
+	<div class = "container">
+		<section id="new_story" class = "input-block">
+			<header>
+				<h2>Add a New Story</h2>
+			</header>
+			<form method="post" action="../actions/action_add_story.php">
+				<input type="hidden" name="csrf" value="<?=$_SESSION['csrf']?>">
+				<input type="text" name="title" placeholder="Title" required>
+				<textarea name="story" placeholder="Story" required></textarea>
+				<?php draw_channel_options($channels); ?>
+				<input type="submit" value="Add Story">
+			</form>
+			<?php if(isset($_GET['error'])){ 
+					if($_GET['error'] == 'title'){?>
+						<h3>Title is too long! (Max is 50 characters)</h3>
+			<?php } else if($_GET['error'] == 'text') {?>
+						<h3>Story is too long! (Max is 50000 characters)</h3>
+			<?php }} ?>
+		</section>
+	</div>
 <?php draw_footer();?>
