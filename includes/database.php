65346d46b432e0b8b7a75f2ae30a977f516cef75
Rekicho <up201604145@fe.up.pt>
2018-12-17 02:03:51 +0000

    Minor changes for delivery

--- includes/database.php
+++ includes/database.php
@@ -1,4 +1,5 @@
 <?php
+	//Copied from https://github.com/arestivo/fare-niente/blob/master/includes/database.php
     /**
      * A singleton representing the app connection 
      * to the database. Use Database::instance() to 
